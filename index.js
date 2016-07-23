c311aa54c45cb11075c86650da99f21b58b0aa19
Sven Eberth <Sven.Eberth@gmail.com>
2016-07-23 22:31:37 +0200

    cleaned up

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
-var gulp = require('gulp');
-var rename = require('gulp-rename');
+"use strict";
 
-var path = require('path');
-var isThere = require('is-there');
-var fs = require('fs');
-var mkdirp = require('mkdirp');
+const PLUGIN_NAME = 'viur-ignite-html';
 
-
-htmlDir = "./sources/html/"
-appengineDir = "./appengine/html/"
+var path = require('path'),
+	isThere = require('is-there'),
+	fs = require('fs'),
+	mkdirp = require('mkdirp');
 
 
 module.exports = {
-	build: function() {
-		var	menus = fs.readFileSync(htmlDir+"_menu.json", 'utf8');
-			menus = JSON.parse(menus); // parse json
+	build: function(options) {
 
+		// Set Default Options
+		var defaultOptions = {
+			src: './sources/html/',
+			dest: './appengine/html/'
+		};
+
+		if (typeof(options)==='undefined') options = {};
+		for (var key in defaultOptions) {
+			if (typeof(options[key])==='undefined') options[key] = defaultOptions[key];
+		}
+
+
+		// Get menu(s) and parse as js dictionary
+		var	menus = fs.readFileSync(options.src+"_menu.json", 'utf8');
+			menus = JSON.parse(menus);
+
+
+		// create an array with all sites of all menus
 		var allSites = {}
 		for(var menu in menus) {
 			var sites = menus[menu];
 			for(var item in sites) {
-				allSites[item] = menus[menu][item]; // create a array with all sites of all menus
+				allSites[item] = menus[menu][item];
 			}
 		}
 
 
-		var layout = fs.readFileSync(htmlDir+"_layout.html", 'utf8'); // get template
+		// get template
+		var layout = fs.readFileSync(options.src+"_layout.html", 'utf8');
 
 
 		for(var site in allSites) { // for each sites
@@ -34,7 +48,7 @@ module.exports = {
 			var siteType = site.split('_')[1];
 
 
-			console.log("Processing %s", siteName)
+			console.log("Processing %s", siteName);
 
 
 			// get content of file
@@ -50,53 +64,51 @@ module.exports = {
 				var sites = menus[menu];
 				var	tmpMenu = ''
 
+
 				if(menuType == 'Sort')
 					sites = sortProperties(sites) // Sort sites by value alphabetically if suffix == sort
+
 				
 				for(var tmpSite in sites) { // for each site in this menu build menu item
 					var tmpSiteTitle = sites[tmpSite];
 					var tmpSiteName = tmpSite.split('_')[0];
 					var tmpSiteType = tmpSite.split('_')[1];
 					
-					tmpMenu += '<li class="menu-item ' + menuName + '-item"><a class="menu-link' + (tmpSiteType == 'Primary' ? ' is-primary' : '') + (siteName == tmpSiteName ? ' is-active' : '') + '" href="' + tmpSiteName + '">' + tmpSiteTitle+ '</a></li>' + '\n'
+					tmpMenu += '<li class="menu-item ' + menuName + '-item"><a class="menu-link' + (tmpSiteType == 'Primary' ? ' is-primary' : '') + (siteName == tmpSiteName ? ' is-active' : '') + '" href="' + tmpSiteName + '">' + tmpSiteTitle+ '</a></li>' + '\n';
 				}
 
 				tmp = tmp.replace( '{{'+menuName+'}}', tmpMenu ) // replace menu variable by menu items
 			}
 
 
-			// create folder appengine/html if doesn't exists
-			if(!isThere(appengineDir)) {
-				mkdirp(appengineDir, function (err) {
-					if (err) return console.error(err)
+			// create destination folder if doesn't exists
+			if(!isThere(options.dest)) {
+				mkdirp(options.dest, function (err) {
+					if (err) return console.error(err);
 				});
 			}
 			
-			// write file in appengine/html
-			fs.writeFile(appengineDir + siteName, tmp, function(err) {
-				if(err) return console.log(err);
+			// write file in destination folder
+			fs.writeFile(options.dest + siteName, tmp, function(err) {
+				if(err) return console.error(err);
 			}); 
+
+			return true;
 		}
-		
 	}
 };
 
-function dirname(path) {
-	return path.replace(/\\/g, '/')
-		.replace(/\/[^\/]*\/?$/, '');
-}
-
 function sortProperties(obj) {
+	var sortable = [];
+	var dict = {};
+
 	// convert object into array
-	var sortable=[];
-	var dict={};
 	for(var key in obj)
 		if(obj.hasOwnProperty(key))
 			sortable.push([key, obj[key]]); // each item is an array in format [key, value]
 
 	// sort items by value
-	sortable.sort(function(a, b)
-	{
+	sortable.sort(function(a, b) {
 		var x=a[1].toLowerCase(),
 			y=b[1].toLowerCase();
 		return x<y ? -1 : x>y ? 1 : 0;
@@ -104,7 +116,8 @@ function sortProperties(obj) {
 
 
 	for(var key in sortable) {
-		dict[sortable[key][0]] = sortable[key][1]
+		dict[sortable[key][0]] = sortable[key][1] // parse array to dictionary
 	}
-	return dict; // array in format [ [ key1, val1 ], [ key2, val2 ], ... ]
+
+	return dict; // sorted dictionary
 }
